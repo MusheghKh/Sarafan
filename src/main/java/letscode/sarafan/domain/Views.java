becad9f5aee2c51f16a1d0e3306cf48bd566de85
Mushegh <asddsajpg@gmail.com>
2021-03-21 21:48:31 +0400

    Connecting DB

---
+++ src/main/java/letscode/sarafan/domain/Views.java
@@ -0,0 +1,10 @@
+package letscode.sarafan.domain;
+
+public class Views {
+
+    public interface Id {}
+
+    public interface IdText extends Id {}
+
+    public interface FullMessage extends Id {}
+}
