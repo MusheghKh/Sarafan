fed20c5eec1a7ed3b4f74867a58fdb382fde7823
Mushegh <asddsajpg@gmail.com>
2021-03-28 16:00:05 +0400

    websocket

--- src/main/java/letscode/sarafan/domain/User.java
+++ src/main/java/letscode/sarafan/domain/User.java
@@ -1,16 +1,18 @@
 package letscode.sarafan.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "usr")
 @Data
-public class User {
+public class User implements Serializable {
     @Id
     private String id;
 
@@ -24,5 +26,6 @@ public class User {
 
     private String locale;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastVisit;
 }
